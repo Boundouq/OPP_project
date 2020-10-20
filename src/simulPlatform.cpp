edf1f5e895529a1aaeacbea8897f3c44fd6b1188
Kenza <jabrane.ke@gmail.com>
2020-10-21 01:32:38 +0200

    ajout interface

--- src/simulPlatform.cpp
+++ src/simulPlatform.cpp
@@ -13,36 +13,52 @@ int main ( int argc, char *argv[] ) {
     string file_path;
     string command;
     fstream file;
+    int nb_it;
+    string cmd0;
 
-        if ( argc > 2 || (argc < 2) ) {
+        if ( argc > 3 || (argc < 3) ) {
             cout << "\033[1;31mTO MUCH / FEW ARGUMENTS \033[0m" << endl;
-            cout << "\033[1;33mUsage : similPlatform <Platform_path>\033[0m" << endl;
+            cout << "\033[1;33mUsage : similPlatform <Platform_path> <number_iteration>\033[0m" << endl;
             return(0);
         }
-        if(argc == 2) {
+        if(argc == 3) {
           file_path = argv[1];
+          stringstream(argv[2]) >> nb_it;
         }
 
         Platform Pf(file_path);
         file.open(file_path);
-
+        cout << "\033[1;32mEXECUTE ALL NUMBER OF ITERATIONS SIMULATIONS AT ONCE ? <y/n>\033[0m" << endl;
+        cout << "\033[1;36m" << PROMPT_STRING <<"\033[0m";
+        cin >> cmd0;
     /* boucle infinie : lit puis execute une cmd en boucle */
     while ( 1 ) {
       if (file.is_open()){
-        cout << "\033[1;32mTWO OPTION ARE POSSIBLE: > simulate(s) , > quit(q) \033[0m" << endl;
-        cout << "\033[1;36m" << PROMPT_STRING <<"\033[0m";
-        cin >> command;
-        cout <<endl;
-        if (command == "simulate" || command == "s" )
-          Pf.simulate();
-        else if( command == "quit" || command == "q"){
-          cout << "\033[1;32mBye bye ! \033[0m" << endl;
-          return(0);
+        if(cmd0 == "y") {
+          for(unsigned int i=0;i<nb_it;i++) Pf.simulate();
+          return 1;
         }
-        else
-          cout << "\033[1;33m==>  Check Your Command !\033[0m" << endl;
+        else if (cmd0 == "n") {
+          cout << "\033[1;32mTWO OPTION ARE POSSIBLE: > simulate(s) , > quit(q) \033[0m" << endl;
+          cout << "\033[1;36m" << PROMPT_STRING <<"\033[0m";
+          cin >> command;
+          cout <<endl;
+          if (command == "simulate" || command == "s" )
+            Pf.simulate();
+          else if( command == "quit" || command == "q"){
+            cout << "\033[1;32mBye bye ! \033[0m" << endl;
+            return(0);
+          }
+          else
+            cout << "\033[1;33m==>  Check Your Command !\033[0m" << endl;
+        }
+        else{
+          cout << "\033[1;31mNon Compatible Command !\033[0m" << endl;
+          return 0;
+        }
       }
-      else cout<<"erreur" << endl;
+
+      else cout << "\033[1;31mERR0R! \033[0m" << endl;
     }
     cout << "\033[1;31mSHOULD NEVER BE HERE \033[0m" << endl;
 }
